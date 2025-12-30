d58c5b595ea9c8002ab3e9a2e26bd502f0607a87
unknown <karkiaku000@gmail.com>
2025-12-30 19:00:22 +0545

    second commit

--- lib/dashboardpage.dart
+++ lib/dashboardpage.dart
@@ -39,7 +39,7 @@ class _MainDashboardPageState extends State<MainDashboardPage> {
             'Logout',
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          content: const Text('Are you sure you want to logout?'),
+          content: const Text('Are you suree you want to logout?'),
           actions: <Widget>[
             TextButton(
               child: const Text('Cancel', style: TextStyle(color: Colors.grey)),
