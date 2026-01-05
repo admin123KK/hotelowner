c8b8642ec590fa8353e109d51066ed6429b007a6
unknown <karkiaku000@gmail.com>
2026-01-05 23:38:22 +0545

    font

--- lib/transctionspage.dart
+++ lib/transctionspage.dart
@@ -268,7 +268,7 @@ class _TransactionsPageState extends State<TransactionsPage> {
                                               color:
                                                   statusColor.withOpacity(0.15),
                                               borderRadius:
-                                                  BorderRadius.circular(30),
+                                                  BorderRadius.circular(29),
                                             ),
                                             child: Text(
                                               status,
